f2356e1b31c6ae8969df4dcf6b4ba8a3b8cd55e1
essde342-bit <essde342@gmail.com>
2026-09-19 21:28:47 +0300

    Add Fullbright and No Hurt Cam settings

--- src/main/java/com/essde342/triggerbot/TriggerBotConfig.java
+++ src/main/java/com/essde342/triggerbot/TriggerBotConfig.java
@@ -10,4 +10,9 @@ public class TriggerBotConfig {
     public int optimizationLevel = 2; // 0=Balanced, 1=Performance, 2=Extreme
     public boolean adaptiveOptimization = false;
     public int targetFps = 45;
+
+    // Visual modules.
+    public boolean fullbright = false;
+    public boolean noHurtCam = true;
+    public double fullbrightGamma = 15.0D;
 }
