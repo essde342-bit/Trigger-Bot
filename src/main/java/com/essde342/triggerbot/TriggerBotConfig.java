877947ed1e93cf9272fe62d8821604060fe3826b
essde342-bit <essde342@gmail.com>
2026-09-19 19:25:54 +0300

    Add config model

---
+++ src/main/java/com/essde342/triggerbot/TriggerBotConfig.java
@@ -0,0 +1,7 @@
+package com.essde342.triggerbot;
+
+public class TriggerBotConfig {
+    public boolean enabled = true;
+    public boolean onlyCrits = false;
+    public boolean onlyWeapon = false;
+}
