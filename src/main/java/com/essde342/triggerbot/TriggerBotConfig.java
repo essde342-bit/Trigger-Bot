ee9ac6673dde9e15e4569fa2f0321b8ad32f1d08
essde342-bit <essde342@gmail.com>
2026-09-19 20:05:59 +0300

    Add TriggerBot optimization setting

--- src/main/java/com/essde342/triggerbot/TriggerBotConfig.java
+++ src/main/java/com/essde342/triggerbot/TriggerBotConfig.java
@@ -4,4 +4,6 @@ public class TriggerBotConfig {
     public boolean enabled = true;
     public boolean onlyCrits = false;
     public boolean onlyWeapon = false;
+    // Reduces client-side TriggerBot checks from every tick to every 2 ticks.
+    public boolean optimization = true;
 }
