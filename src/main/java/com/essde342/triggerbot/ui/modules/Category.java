03d38ae6687eab6db4baf5a076223332bbfefcc9
To4noHe_BadBoy <essde342@gmail.com>
2026-09-20 13:38:10 +0300

    Organize GUI into Combat Visuals and Other tabs

--- src/main/java/com/essde342/triggerbot/ui/modules/Category.java
+++ src/main/java/com/essde342/triggerbot/ui/modules/Category.java
@@ -1,1 +1,17 @@
-package com.essde342.triggerbot.ui.modules; public enum Category{COMBAT,MOVEMENT,RENDER,PLAYER,WORLD}
+package com.essde342.triggerbot.ui.modules;
+
+public enum Category {
+    COMBAT("Combat"),
+    VISUALS("Visuals"),
+    OTHER("Other");
+
+    private final String displayName;
+
+    Category(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
