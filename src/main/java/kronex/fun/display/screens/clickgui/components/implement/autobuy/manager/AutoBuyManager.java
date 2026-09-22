47ed01fad1dcd1b374f619d4ce45351fe97181ea
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:09:22 +0300

    Port ClickGUI AutoBuy support: AutoBuyManager.java

---
+++ src/main/java/kronex/fun/display/screens/clickgui/components/implement/autobuy/manager/AutoBuyManager.java
@@ -0,0 +1,16 @@
+package kronex.fun.display.screens.clickgui.components.implement.autobuy.manager;
+
+import kronex.fun.display.screens.clickgui.components.implement.autobuy.items.AutoBuyableItem;
+import kronex.fun.display.screens.clickgui.components.implement.autobuy.originalitems.ItemRegistry;
+import java.util.List;
+
+public class AutoBuyManager {
+    private static AutoBuyManager instance;
+    private boolean enabled = false;
+    private AutoBuyManager() {}
+    public static AutoBuyManager getInstance() { if (instance == null) instance = new AutoBuyManager(); return instance; }
+    public void setEnabled(boolean enabled) { this.enabled = enabled; }
+    public boolean isEnabled() { return enabled; }
+    public List<AutoBuyableItem> getAllItems() { return ItemRegistry.getAllItems(); }
+    public void toggleItem(AutoBuyableItem item) { item.setEnabled(!item.isEnabled()); }
+}
