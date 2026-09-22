e8d63ca202233e7575f5ab9d6878419612eeba1e
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:11:46 +0300

    Port AutoBuy item providers

---
+++ src/main/java/kronex/fun/display/screens/clickgui/components/implement/autobuy/items/list/KrushItems.java
@@ -0,0 +1,17 @@
+package kronex.fun.display.screens.clickgui.components.implement.autobuy.items.list;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+public final class KrushItems {
+    private KrushItems(){}
+    public static ItemStack getHelmet(){return new ItemStack(Items.NETHERITE_HELMET);}
+    public static ItemStack getChestplate(){return new ItemStack(Items.NETHERITE_CHESTPLATE);}
+    public static ItemStack getLeggings(){return new ItemStack(Items.NETHERITE_LEGGINGS);}
+    public static ItemStack getBoots(){return new ItemStack(Items.NETHERITE_BOOTS);}
+    public static ItemStack getSword(){return new ItemStack(Items.NETHERITE_SWORD);}
+    public static ItemStack getPickaxe(){return new ItemStack(Items.NETHERITE_PICKAXE);}
+    public static ItemStack getCrossbow(){return new ItemStack(Items.CROSSBOW);}
+    public static ItemStack getTrident(){return new ItemStack(Items.TRIDENT);}
+    public static ItemStack getMace(){return new ItemStack(Items.MACE);}
+}
