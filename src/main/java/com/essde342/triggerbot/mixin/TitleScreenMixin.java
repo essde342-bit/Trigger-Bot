19a9afecb93c0905771a5ab13c9b992560c102fa
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 18:23:41 +0300

    Port TitleScreenMixin.java to Minecraft 1.21.4

--- src/main/java/com/essde342/triggerbot/mixin/TitleScreenMixin.java
+++ src/main/java/com/essde342/triggerbot/mixin/TitleScreenMixin.java
@@ -4,7 +4,7 @@ import com.essde342.triggerbot.AltManagerScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,22 +13,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
     protected TitleScreenMixin() {
-        super(new LiteralText("Minecraft"));
+        super(Text.literal("Minecraft"));
     }
 
     @Inject(method = "init", at = @At("TAIL"))
     private void triggerBot$addAltManagerButton(CallbackInfo info) {
-        this.addButton(new ButtonWidget(
-                this.width - 110,
-                8,
-                102,
-                20,
-                new LiteralText("Alt Manager"),
-                button -> {
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("Alt Manager"), button -> {
                     if (this.client != null) {
-                        this.client.openScreen(AltManagerScreen.create((Screen) (Object) this));
+                        this.client.setScreen(AltManagerScreen.create((Screen) (Object) this));
                     }
-                }
-        ));
+                })
+                .dimensions(this.width - 110, 8, 102, 20)
+                .build());
     }
 }
