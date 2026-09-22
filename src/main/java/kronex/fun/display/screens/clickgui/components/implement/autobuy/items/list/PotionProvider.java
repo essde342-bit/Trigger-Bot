31c34a6e04bfc8e32a5334619a02a2c514b27637
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:09:29 +0300

    Port ClickGUI AutoBuy support: PotionProvider.java

---
+++ src/main/java/kronex/fun/display/screens/clickgui/components/implement/autobuy/items/list/PotionProvider.java
@@ -0,0 +1,9 @@
+package kronex.fun.display.screens.clickgui.components.implement.autobuy.items.list;
+
+import kronex.fun.display.screens.clickgui.components.implement.autobuy.items.AutoBuyableItem;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PotionProvider {
+    public static List<AutoBuyableItem> getPotions() { return new ArrayList<>(); }
+}
