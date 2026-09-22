6e6f6ee6b71b61ad80cfc7da484aa932be2bdaac
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:11:52 +0300

    Port AutoBuy item providers

---
+++ src/main/java/kronex/fun/display/screens/clickgui/components/implement/autobuy/items/list/DonatorProvider.java
@@ -0,0 +1,17 @@
+package kronex.fun.display.screens.clickgui.components.implement.autobuy.items.list;
+
+import kronex.fun.display.screens.clickgui.components.implement.autobuy.items.AutoBuyableItem;
+import kronex.fun.display.screens.clickgui.components.implement.autobuy.items.customitem.CustomItem;
+import kronex.fun.display.screens.clickgui.components.implement.autobuy.items.defaultsetpricec.Defaultpricec;
+import net.minecraft.item.Items;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DonatorProvider {
+    public static List<AutoBuyableItem> getDonator(){
+        List<AutoBuyableItem> r=new ArrayList<>();
+        r.add(new CustomItem("[★] Элитры","[★] Элитры","Элитры",null,Items.ELYTRA,Defaultpricec.getPrice("Элитры")));
+        r.add(new CustomItem("[★] Тотем бессмертия","[★] Тотем бессмертия","Тотем бессмертия",null,Items.TOTEM_OF_UNDYING,Defaultpricec.getPrice("Тотем бессмертия")));
+        return r;
+    }
+}
