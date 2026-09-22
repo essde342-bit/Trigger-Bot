7d78f3d12e2b31094ecde42de7e223737050aab0
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 19:57:02 +0300

    Port original ClickGUI component: components/implement/settings/SButtonComponent.java

---
+++ src/main/java/kronex/fun/display/screens/clickgui/components/implement/settings/SButtonComponent.java
@@ -0,0 +1,19 @@
+package kronex.fun.display.screens.clickgui.components.implement.settings;
+
+import net.minecraft.client.gui.DrawContext;
+import kronex.fun.features.module.setting.implement.ButtonSetting;
+import kronex.fun.other.utils.display.font.Fonts;
+import kronex.fun.display.screens.clickgui.components.implement.other.ButtonComponent;
+import static kronex.fun.other.utils.display.font.Fonts.Type.BOLD;
+
+public class SButtonComponent extends AbstractSettingComponent {
+ private final ButtonComponent buttonComponent=new ButtonComponent(); private final ButtonSetting setting;
+ public SButtonComponent(ButtonSetting setting){super(setting);this.setting=setting;}
+ @Override public void render(DrawContext context,int mouseX,int mouseY,float delta){
+  var wrapped=setting.getDescription(); height=(int)(18+Fonts.getSize(12,Fonts.Type.DEFAULT).getStringHeight(wrapped)/3);
+  Fonts.getSize(14,BOLD).drawString(context.getMatrices(),setting.getName(),x+9,y+6,0xFFD4D6E1);
+  Fonts.getSize(12,Fonts.Type.DEFAULT).drawString(context.getMatrices(),wrapped,x+9,y+15,0xFF878894);
+  ((ButtonComponent)buttonComponent.setText("Click on me").setRunnable(setting.getRunnable()).position(x+width-9-buttonComponent.width,y+5)).render(context,mouseX,mouseY,delta);
+ }
+ @Override public boolean mouseClicked(double mouseX,double mouseY,int button){buttonComponent.mouseClicked(mouseX,mouseY,button);return super.mouseClicked(mouseX,mouseY,button);}
+}
