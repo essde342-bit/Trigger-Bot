f6f1611767e3816fb675202d18f74c361ae215b4
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:47:07 +0300

    Fix remaining ClickGUI port compile errors

--- src/main/java/kronex/fun/display/screens/clickgui/components/implement/autobuy/items/list/MiscProvider.java
+++ src/main/java/kronex/fun/display/screens/clickgui/components/implement/autobuy/items/list/MiscProvider.java
@@ -11,7 +11,7 @@ public class MiscProvider {
     public static List<AutoBuyableItem> getMisc(){
         List<AutoBuyableItem> r=new ArrayList<>();
         String[][] values={{"Алмаз","Алмаз","Алмаз"},{"Золотое яблоко","Золотое яблоко","Золотое яблоко"},{"Незеритовый слиток","Незеритовый слиток","Незеритовый слиток"},{"Тотем бессмертия","Тотем бессмертия","Тотем бессмертия"}};
-        for(String[] v:values)r.add(new CustomItem(v[0],v[1],v[2],null,switch(v[0]){case "Алмаз"->Items.DIAMOND;case "Золотое яблоко"->Items.ENCHANTED_GOLDEN_APPLE;case "Незеритовый слиток"->Items.NETHERITE_INGOT;default->Items.TOTEM_OF_UNDYING;},Defaultpricec.getPrice(v[2])));
+        for(String[] v:values)r.add(new CustomItem(v[0],v[1],v[2],null,switch(v[0]){case "Алмаз"->Items.DIAMOND;case "Золотое яблоко"->Items.ENCHANTED_GOLDEN_APPLE;case "Незеритовый слиток"->Items.NETHERITE_INGOT;default->Items.TOTEM_OF_UNDYING;},Defaultpricec.getPrice(v[2]),null,null));
         return r;
     }
 }
