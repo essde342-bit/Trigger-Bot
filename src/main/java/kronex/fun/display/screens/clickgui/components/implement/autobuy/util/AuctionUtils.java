50f99d9110f5af815b9f39492881aca55b7186a5
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:38:37 +0300

    Correct AuctionUtils regex escaping

--- src/main/java/kronex/fun/display/screens/clickgui/components/implement/autobuy/util/AuctionUtils.java
+++ src/main/java/kronex/fun/display/screens/clickgui/components/implement/autobuy/util/AuctionUtils.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class AuctionUtils {
-    public static final Pattern funTimePricePattern = Pattern.compile("\\$(\\d+(?:[\\\\s,]\\d{3})*(?:\\.\\d{2})?)");
+    public static final Pattern funTimePricePattern = Pattern.compile("\\$(\\d+(?:[\\s,]\\d{3})*(?:\\.\\d{2})?)");
 
     private AuctionUtils() {}
 
