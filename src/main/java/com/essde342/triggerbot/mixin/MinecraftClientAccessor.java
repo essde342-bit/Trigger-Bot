da18f61438aea38e0f9fcfe3d0f16ebfb4f316e9
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 18:23:10 +0300

    Port MinecraftClient session accessor to 1.21.4

--- src/main/java/com/essde342/triggerbot/mixin/MinecraftClientAccessor.java
+++ src/main/java/com/essde342/triggerbot/mixin/MinecraftClientAccessor.java
@@ -1,7 +1,7 @@
 package com.essde342.triggerbot.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.util.Session;
+import net.minecraft.client.session.Session;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
