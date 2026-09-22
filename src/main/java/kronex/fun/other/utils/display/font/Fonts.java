8540e3079f7262d435f54c75d63c58e5c9e3677b
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:14:05 +0300

    Add archive font variants for ClickGUI

--- src/main/java/kronex/fun/other/utils/display/font/Fonts.java
+++ src/main/java/kronex/fun/other/utils/display/font/Fonts.java
@@ -2,18 +2,9 @@ package kronex.fun.other.utils.display.font;
 
 public final class Fonts {
     private Fonts() {}
-
     public enum Type {
-        DEFAULT,
-        BOLD,
-        HUD
+        DEFAULT, BOLD, HUD, SEMI, REGULAR, ICONS2
     }
-
-    public static FontRenderer getSize(float size, Type type) {
-        return new FontRenderer(size);
-    }
-
-    public static FontRenderer getSize(int size, Type type) {
-        return new FontRenderer(size);
-    }
+    public static FontRenderer getSize(float size, Type type) { return new FontRenderer(size); }
+    public static FontRenderer getSize(int size, Type type) { return new FontRenderer(size); }
 }
