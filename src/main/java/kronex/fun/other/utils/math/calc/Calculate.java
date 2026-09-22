f1049b83c13f5887e6ab7716c40298d06fe1e5dc
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:14:40 +0300

    Expose smooth interpolation for ClickGUI

--- src/main/java/kronex/fun/other/utils/math/calc/Calculate.java
+++ src/main/java/kronex/fun/other/utils/math/calc/Calculate.java
@@ -3,13 +3,9 @@ package kronex.fun.other.utils.math.calc;
 import kronex.fun.other.utils.math.MathUtil;
 
 public final class Calculate {
-    private Calculate() {}
-
-    public static boolean isHovered(double mouseX, double mouseY, double x, double y, double width, double height) {
-        return MathUtil.isHovered(mouseX, mouseY, x, y, width, height);
-    }
-
-    public static float interpolate(float current, float target) {
-        return (float) MathUtil.interpolate(current, target);
-    }
+    private Calculate(){}
+    public static boolean isHovered(double mouseX,double mouseY,double x,double y,double width,double height){return MathUtil.isHovered(mouseX,mouseY,x,y,width,height);}
+    public static float interpolate(float current,float target){return (float)MathUtil.interpolate(current,target);}
+    public static float interpolateSmooth(float speed,float current,float target){return MathUtil.interpolateSmooth(speed,current,target);}
+    public static float interpolateSmooth(double speed,float current,float target){return MathUtil.interpolateSmooth(speed,current,target);}
 }
