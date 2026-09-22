03a4bfe9d85272c6249eb5fb862f9f9e7bae3fb2
To4noHe_BadBoy <essde342@gmail.com>
2026-09-22 20:15:22 +0300

    Add bind compatibility to boolean GUI settings

--- src/main/java/kronex/fun/features/module/setting/implement/BooleanSetting.java
+++ src/main/java/kronex/fun/features/module/setting/implement/BooleanSetting.java
@@ -4,29 +4,15 @@ import kronex.fun.features.module.setting.Setting;
 
 public final class BooleanSetting extends Setting {
     private final com.essde342.triggerbot.ui.imple.BooleanSetting backend;
+    private int key=-1;
+    private int type;
 
-    public BooleanSetting(
-            String name,
-            com.essde342.triggerbot.ui.imple.BooleanSetting backend
-    ) {
-        super(name, "");
-        this.backend = backend;
-    }
-
-    public BooleanSetting(
-            String name,
-            String description,
-            com.essde342.triggerbot.ui.imple.BooleanSetting backend
-    ) {
-        super(name, description);
-        this.backend = backend;
-    }
-
-    public boolean isValue() {
-        return backend.isEnabled();
-    }
-
-    public void setValue(boolean value) {
-        backend.setEnabled(value);
-    }
+    public BooleanSetting(String name,com.essde342.triggerbot.ui.imple.BooleanSetting backend){super(name,"");this.backend=backend;}
+    public BooleanSetting(String name,String description,com.essde342.triggerbot.ui.imple.BooleanSetting backend){super(name,description);this.backend=backend;}
+    public boolean isValue(){return backend.isEnabled();}
+    public void setValue(boolean value){backend.setEnabled(value);}
+    public int getKey(){return key;}
+    public void setKey(int key){this.key=key;}
+    public int getType(){return type;}
+    public void setType(int type){this.type=type;}
 }
