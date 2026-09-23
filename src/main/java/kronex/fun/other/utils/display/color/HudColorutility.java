c687db470fdbafaa57b2c3417330f341e0af9fd3
To4noHe_BadBoy <essde342@gmail.com>
2026-09-23 21:14:37 +0300

    Align HUD palette with ClickGUI

--- src/main/java/kronex/fun/other/utils/display/color/HudColorutility.java
+++ src/main/java/kronex/fun/other/utils/display/color/HudColorutility.java
@@ -1,7 +1,13 @@
 package kronex.fun.other.utils.display.color;
 
 public final class HudColorutility {
-    private HudColorutility(){}
-    public static int getRectGradient(float a,float b){return ColorAssist.applyOpacity(0xFF161920,235);}
-    public static int getIconColor(){return ColorAssist.getClientColor();}
+    private HudColorutility() {}
+
+    public static int getRectGradient(float a, float b) {
+        return ColorAssist.applyOpacity(ColorAssist.PANEL, 245);
+    }
+
+    public static int getIconColor() {
+        return ColorAssist.getClientColor();
+    }
 }
