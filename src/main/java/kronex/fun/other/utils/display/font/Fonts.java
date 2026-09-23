d23e562f40f273f539b3fae72df4b190d5da8236
To4noHe_BadBoy <essde342@gmail.com>
2026-09-23 21:14:11 +0300

    Use font styles in ClickGUI renderer

--- src/main/java/kronex/fun/other/utils/display/font/Fonts.java
+++ src/main/java/kronex/fun/other/utils/display/font/Fonts.java
@@ -2,9 +2,16 @@ package kronex.fun.other.utils.display.font;
 
 public final class Fonts {
     private Fonts() {}
+
     public enum Type {
         DEFAULT, BOLD, HUD, SEMI, REGULAR, ICONS2
     }
-    public static FontRenderer getSize(float size, Type type) { return new FontRenderer(size); }
-    public static FontRenderer getSize(int size, Type type) { return new FontRenderer(size); }
+
+    public static FontRenderer getSize(float size, Type type) {
+        return new FontRenderer(size, type);
+    }
+
+    public static FontRenderer getSize(int size, Type type) {
+        return new FontRenderer(size, type);
+    }
 }
